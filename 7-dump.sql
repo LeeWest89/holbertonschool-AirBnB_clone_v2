16f644e79c68ae6eb9d198bccd433e70b38aff19
LeeWest89 <6683@holbertonstudents.com>
2023-11-13 12:30:52 -0600

    final push i hope

--- 7-dump.sql
+++ 7-dump.sql
@@ -37,9 +37,9 @@ DROP TABLE IF EXISTS `amenities`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `amenities` (
   `id` varchar(60) NOT NULL,
-  `name` varchar(128) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
